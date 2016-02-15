b21181137ca57c0ef3398314bca7e67f56703bf8
Becky Beauchamp <beckybeauchamp1@gmail.com>
2016-02-15 09:21:41 -0500

    Saves Inline Comments

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 // There definitely are a couple of different issues with this code. One example is that,
 // you are trying to use jQuery and you don't have jQuery or a CDN script tag in your html document
-// I would like to go over this again when we start jQuery. For now, just practice Vanilla JS!!  
+// I would like to go over this again when we start jQuery. For now, just practice Vanilla JS!!
 $('#list').hover(function() {
     $('ul', this).addClass('pseudoHover');
 });
